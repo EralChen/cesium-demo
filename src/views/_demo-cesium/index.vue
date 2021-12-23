f6be8f09f84caf0c9c19b815d6dfda501d501b3a
Eral Chen <eralchen@foxmail.com>
2021-12-23 17:10:24 +0800

    cesium demo

--- src/views/_demo-cesium/index.vue
+++ src/views/_demo-cesium/index.vue
@@ -1,17 +1,29 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { VumViewer, VumSkyBasemap } from 'vuesium'
+import { VumViewer, VumSkyBasemap, VumCesium3dTileset, __VumCesium3dTileset } from 'vuesium'
 export default defineComponent({
   components: {
-    VumViewer,VumSkyBasemap,
+    VumViewer,VumSkyBasemap,VumCesium3dTileset,
   },
   setup () {
-    return {}
+    const tileLoad:__VumCesium3dTileset.OnLoad = ({viewer, tileset}) => {
+      viewer.zoomTo(tileset)
+    }
+    return {
+      tileLoad,
+    }
   },
 })
 </script>
 <template>
   <VumViewer>
     <VumSkyBasemap></VumSkyBasemap>
+    <VumCesium3dTileset :default-options="{
+      url: 'http://112.16.181.129:180/web/tileset/wlsq/djsq/tileset.json'
+    }"
+      @load="tileLoad"
+    >
+
+    </VumCesium3dTileset>
   </VumViewer>
 </template>
