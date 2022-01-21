e2237337e34a85c4fbeeb83dcc0e258956259d90
Eral Chen <eralchen@foxmail.com>
2022-01-21 15:04:02 +0800

    home

--- src/views/_demo-el-plus.admin/home/index.vue
+++ src/views/_demo-el-plus.admin/home/index.vue
@@ -1,19 +1,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
-import moment from 'moment'
 export default defineComponent({
   setup () {
-    const endtime = moment.utc('2022/1/16 07:21')
-    const startTime = moment.utc()
-    const diffTime = endtime.diff(startTime, 'seconds')
-    const twoDaysFromNow = (new Date().getTime() / 1000) + diffTime + 1
-    return {
-      twoDaysFromNow,
-    }
+    return {}
   },
 })
 </script>
 <template>
-  {{ twoDaysFromNow }}
   <div>home</div>
 </template>
