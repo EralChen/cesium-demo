6681d694b267ce10c490f5130c5b5cd2f14ab442
Eral Chen <eralchen@foxmail.com>
2022-02-07 15:47:04 +0800

    close

--- src/layout/admin/header/index.vue
+++ src/layout/admin/header/index.vue
@@ -1,13 +1,17 @@
 <script lang="ts">
-import { defineComponent, ref, watch } from 'vue'
+import { computed, defineComponent, ref, watch } from 'vue'
 import { useRoute, RouteLocationNormalizedLoaded, useRouter } from 'vue-router'
+import { VkBoxHandler } from 'vunk'
 type ViewRoute = Omit<RouteLocationNormalizedLoaded, 'redirectedFrom' | 'matched'>
 export default defineComponent({
+  components: {
+    VkBoxHandler,
+  },
   setup () {
     const route = useRoute()
     const router = useRouter()
     const visitedViews = ref<Map<string, ViewRoute>>(new Map())
-    
+    const visitedViewsSize = computed(() => visitedViews.value.size)
     watch(route, (v) => {
       const doc = JSON.parse(JSON.stringify({
         fullPath: v.fullPath.endsWith('/') ? v.fullPath.slice(0, -1) : v.fullPath,
@@ -22,12 +26,12 @@ export default defineComponent({
     }, { immediate: true })
 
     const delVisitedViews = (key: string, isExactActive: boolean) => {
-      
+
       [...visitedViews.value].some(([k], index, arr) => {
         if (k === key) {
           visitedViews.value.delete(key)
           if (isExactActive) {
-            const pre =  arr[index + 1 >= arr.length ? index - 1 : index + 1][1]
+            const pre = arr[index + 1 >= arr.length ? index - 1 : index + 1][1]
             router.push(pre)
           }
           return true
@@ -39,6 +43,7 @@ export default defineComponent({
     return {
       visitedViews,
       delVisitedViews,
+      visitedViewsSize,
     }
   },
 })
@@ -47,27 +52,47 @@ export default defineComponent({
   <ul class="layout-admin-header-link-ul" sk-flex="row">
     <li v-for="([key, item]) of visitedViews" :key="key">
       <router-link :to="item.fullPath">
-        <template #default="{ isExactActive  }">
+        <template #default="{ isExactActive }">
+          <VkBoxHandler :class="'r'">
+            <svg-icon
+              :icon-class="'close'"
+              v-show="visitedViewsSize - 1"
+              @click.prevent="delVisitedViews(key, isExactActive)"
+            >错</svg-icon>
+          </VkBoxHandler>
           {{ item.meta.title }}
-          <button @click.prevent="delVisitedViews(key, isExactActive)">错</button>
         </template>
-
-      </router-link> 
-
+      </router-link>
     </li>
   </ul>
 </template>
 <style lang="scss">
-.layout-admin-header-link-ul{
+.layout-admin-header-link-ul {
   background-color: var(--c-bg);
-  padding: .5em;
-  li{
-    a{
+  padding: 0.5em;
+  li {
+    .icon-close {
+      background-color: var(--c-border);
+      border-radius: 50%;
+      color: var(--c-content);
+      opacity: 0;
+      transition: all 0.4s ease;
+    }
+
+    &:hover {
+      .icon-close {
+        opacity: 0.9;
+        display: initial;
+      }
+    }
+
+    a {
+      position: relative;
       border-radius: 4px;
       display: block;
       border: 1px solid var(--c-border);
-      padding: .2em .4em;
-      &.router-link-exact-active{
+      padding: 0.2em 0.4em;
+      &.router-link-exact-active {
         color: var(--ic-content);
         background: var(--c-info);
       }
