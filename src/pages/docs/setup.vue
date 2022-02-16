0bdd851a567efdd6b5b1336010c3e050201a84b2
Eral Chen <eralchen@foxmail.com>
2022-02-16 13:32:21 +0800

    RouterViewWrapper

---
+++ src/pages/docs/setup.vue
@@ -0,0 +1,12 @@
+
+<script>
+import RouterViewWrapper from '_c/RouterViewWrapper/index.vue'
+export default RouterViewWrapper
+</script>
+<route>
+{
+  meta: {
+    title: "Components"
+  }
+}
+</route>
