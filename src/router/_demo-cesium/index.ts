34860f8301a62e40e7cad4df2944bc254a66059e
Eral Chen <eralchen@foxmail.com>
2022-03-11 19:31:14 +0800

    mars3d

--- src/router/_demo-cesium/index.ts
+++ src/router/_demo-cesium/index.ts
@@ -4,4 +4,8 @@ export default [
     path: '/_demo-cesium',
     component: () => import('_v/_demo-cesium/index.vue'),
   },
+  {
+    path: '/_demo-cesium/mars3d',
+    component: () => import('_v/_demo-cesium/mars3d.vue'),
+  },
 ] as RouteRecordRaw[]
---
+++ src/views/_demo-cesium/mars3d.vue
@@ -0,0 +1,49 @@
+
+<script lang="ts">
+import {VumViewer} from 'vuesium'
+import {VumaMap} from 'vuesium-mars3d'
+import { defineComponent } from 'vue'
+import { VumaTilesetLayer, __VumaTilesetLayer } from 'vuesium-mars3d'
+import { Cesium3DTileStyle, CustomShader, LightingModel } from 'cesium'
+export default defineComponent({
+  components: {
+    VumViewer,
+    VumaMap,
+    VumaTilesetLayer,
+  },
+  setup () {
+    const layerLoad:__VumaTilesetLayer.OnLoad = ({layer, map: {viewer}}) => {
+      viewer.flyTo(layer.tileset)
+    }
+    const options:__VumaTilesetLayer.DefaultOptions = {
+      url: 'http://112.16.181.129:180/web/tileset/wlsq/djsq/tileset.json',
+
+      customShader: new CustomShader({
+        lightingModel: LightingModel.UNLIT,
+        isTranslucent: true,
+        fragmentShaderText: `
+      void fragmentMain(FragmentInput fsInput, inout czm_modelMaterial material)
+      {
+        material.diffuse = vec3(1.0, 1.0, 1.0); // 颜色
+        material.alpha = 0.5;
+      } `}),
+      
+      
+    } as any
+    return {
+      layerLoad,
+      options,
+    }
+  },
+})
+</script>
+<template>
+  <vum-viewer>
+    <vuma-map>
+      <vuma-tileset-layer 
+        :default-options="options" 
+        @load="layerLoad"
+      ></vuma-tileset-layer>
+    </vuma-map>
+  </vum-viewer>
+</template>
