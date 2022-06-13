3b7569ee127da029279c21b4024bcc1a25ae6946
EralChen <eralchen@foxmail.com>
2022-06-13 13:11:34 +0800

    form demo

--- src/router/_demo-el-plus/index.ts
+++ src/router/_demo-el-plus/index.ts
@@ -4,4 +4,8 @@ export default [
     path: '/_demo-el-plus',
     component: () => import('_v/_demo-el-plus/index.vue'),
   },
+  {
+    path: '/_demo-el-plus/form',
+    component: () => import('_v/_demo-el-plus/form.vue')
+  }
 ] as RouteRecordRaw[]
